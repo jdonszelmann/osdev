c697580a05334671f1a70eabf795f57016f3c241
Julius de Jeu <jpdejeu@hotmail.nl>
2018-12-24 20:41:05 +0100

    Added RTC clock capabilities, not sure if year autodetection works tho..

--- src/x86_64/kernel/system/main.c
+++ src/x86_64/kernel/system/main.c
@@ -4,6 +4,7 @@
 #include <kernel.h>
 #include <keyboard.h>
 #include <multiboot2.h>
+#include <rtc.h>
 #include <serial.h>
 #include <system.h>
 #include <timer.h>
@@ -23,7 +24,15 @@ void kmain(void *multiboot_structure) {
 
 	printf("test\n");
 	sleep(1000);
-	printf("after 1 ms");
+	printf("after 1 ms\n");
+
+	while(true) {
+		datetime_t dt = get_datetime();
+
+		printf("%i-%i-%i %i:%i:%i\n", dt.year, dt.month, dt.day, dt.hour,
+			   dt.minute, dt.second);
+		sleep(1000);
+	}
 
 	// clang-format off
 	for(;;);
